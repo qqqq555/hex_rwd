b927d0125613ecc3833b3a0d41d653f386b5e75a
eating <eva930514@gmail.com>
2024-05-30 16:06:29 +0800

    finish except register

---
+++ js/all.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.fa-heart').click(function(event){
+        $(this).toggleClass('fa-solid fa-regular');
+    });
+    $('.menu-toggle').click(function() {
+        $('nav ul').toggle();
+    });
+    $('.to-register').click(function(e) {
+        e.preventDefault();
+        $('.login').removeClass('active-form');
+        $('.register').addClass('active-form');
+    });
+
+    $('.to-login').click(function(e) {
+        e.preventDefault();
+        $('.register').removeClass('active-form');
+        $('.login').addClass('active-form');
+    });
+
+})
